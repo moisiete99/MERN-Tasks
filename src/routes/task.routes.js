af16873426b0e9fe2c1fb93a779cda856fb80aa9
Moy <moy_rub@hotmail.com>
2022-07-20 13:40:12 -0500

    instalando mongoose y conectando DB

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router(); //para ingresar rutas
+const Task = require('../models/task')
 
 router.get("/", (req, res) => {
-  res.json({
-   status: 'API works'
-  });
+   Task.find((err, tasks) => {
+      console.log(tasks)
+      res.json({
+       status: 'API works'
+      });
+   })
+
 });
 
 module.exports = router;
