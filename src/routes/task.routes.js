49e3bd5cf785864000b5a39bed37a9f3fa3a7178
Moy <moy_rub@hotmail.com>
2022-07-20 14:10:49 -0500

    ruta POST de model Task

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const router = express.Router(); //para ingresar rutas
-const Task = require('../models/task')
+const Task = require("../models/task");
 
-router.get("/", (req, res) => {
-   Task.find((err, tasks) => {
-      console.log(tasks)
-      res.json({
-       status: 'API works'
-      });
-   })
+router.get("/", async (req, res) => {
+  const tasks = await Task.find();
+  //console.log('received')
+  res.json(tasks);
+});
 
+router.post("/", async (req, res) => {
+  const { title, description } = req.body;
+  const task = new Task({
+    title,
+    description,
+  });
+  await task.save()
+  res.json({status: 'Task saved'});
 });
 
 module.exports = router;
