070f2c9706fa80b36fbc6ece2e029c4297051ce4
Moy <moy_rub@hotmail.com>
2022-07-21 18:22:56 -0500

    card created OK, ver form onSubmit

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,46 @@
 import React, { Component } from "react";
 
-function App() {
+function App () {
+   /* addTask(e) ;{
+      console.log('adding');
+      e.preventDefault();
+   } */
+
    return(
       <div>
-         <h1>Testing</h1>
+         {/* NAVIGATION */}
+         <nav className="light-blue darken-4">
+            <div className="container">
+               <a className="brand-logo" href="/">MERN Stack</a>
+            </div>
+         </nav>
+
+         <div className="container">
+            <div className="row">
+               <div className="col s5">
+                  <div className="card">
+                     <div className="card-content">
+                        <form>
+                           <div className="row">
+                              <div className="input-fiel col s12">
+                                 <input type="text" placeholder="Task Title" />
+                              </div>
+                           </div>
+                           <div className="row">
+                              <div className="input-fiel col s12">
+                                 <textarea placeholder="Task Description" className="materialize-textarea"></textarea>
+                              </div>
+                           </div>
+                           <button type="submit " className="btn light-green darken-4">Send</button>
+                        </form>
+                     </div>
+                  </div>
+               </div>
+               <div className="col s7">
+
+               </div>
+            </div>
+         </div>
       </div>
    )   
 }
