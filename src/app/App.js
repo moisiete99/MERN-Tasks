2a03323fc605754877b2922b294e0151249d2d99
Moy <moy_rub@hotmail.com>
2022-07-22 12:25:50 -0500

    form onSubmit OK

--- src/app/App.js
+++ src/app/App.js
@@ -1,48 +1,50 @@
 import React, { Component } from "react";
 
-function App () {
-   /* addTask(e) ;{
+class App extends Component {
+   addTask(e) {
       console.log('adding');
       e.preventDefault();
-   } */
+   }
 
-   return(
-      <div>
-         {/* NAVIGATION */}
-         <nav className="light-blue darken-4">
+   render() {
+      return(
+         <div>
+            {/* NAVIGATION */}
+            <nav className="light-blue darken-4">
+               <div className="container">
+                  <a className="brand-logo" href="/">MERN Stack</a>
+               </div>
+            </nav>
+   
             <div className="container">
-               <a className="brand-logo" href="/">MERN Stack</a>
-            </div>
-         </nav>
-
-         <div className="container">
-            <div className="row">
-               <div className="col s5">
-                  <div className="card">
-                     <div className="card-content">
-                        <form>
-                           <div className="row">
-                              <div className="input-fiel col s12">
-                                 <input type="text" placeholder="Task Title" />
+               <div className="row">
+                  <div className="col s5">
+                     <div className="card">
+                        <div className="card-content">
+                           <form onSubmit={this.addTask}>
+                              <div className="row">
+                                 <div className="input-fiel col s12">
+                                    <input type="text" placeholder="Task Title" />
+                                 </div>
                               </div>
-                           </div>
-                           <div className="row">
-                              <div className="input-fiel col s12">
-                                 <textarea placeholder="Task Description" className="materialize-textarea"></textarea>
+                              <div className="row">
+                                 <div className="input-fiel col s12">
+                                    <textarea placeholder="Task Description" className="materialize-textarea"></textarea>
+                                 </div>
                               </div>
-                           </div>
-                           <button type="submit " className="btn light-green darken-4">Send</button>
-                        </form>
+                              <button type="submit" className="btn light-green darken-4">Send</button>
+                           </form>
+                        </div>
                      </div>
                   </div>
-               </div>
-               <div className="col s7">
-
+                  <div className="col s7">
+   
+                  </div>
                </div>
             </div>
          </div>
-      </div>
-   )   
+      )
+   }   
 }
 
 export default App
