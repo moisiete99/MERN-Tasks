a7d3defeae8922051232d517759672e832cec016
Moy <moy_rub@hotmail.com>
2022-07-22 15:13:40 -0500

    POST Task method OK

--- src/app/App.js
+++ src/app/App.js
@@ -1,67 +1,94 @@
 import React, { Component } from "react";
 
 class App extends Component {
-   constructor() {
-      super();
-      this.state = {
-        title: '',
-        description: ''
-      };
-      this.handleChange = this.handleChange.bind(this);
-      this.addTask = this.addTask.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      description: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.addTask = this.addTask.bind(this);
+  }
 
-   handleChange(e) {
-      const { name, value } = e.target;
-      this.setState({
-        [name]: value
-      });
-    }
-   
-   addTask(e) {
-      console.log(this.state);
-      e.preventDefault();
-   }
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
 
-   render() {
-      return(
-         <div>
-            {/* NAVIGATION */}
-            <nav className="light-blue darken-4">
-               <div className="container">
-                  <a className="brand-logo" href="/">MERN Stack</a>
-               </div>
-            </nav>
-   
-            <div className="container">
-               <div className="row">
-                  <div className="col s5">
-                     <div className="card">
-                        <div className="card-content">
-                           <form onSubmit={this.addTask}>
-                              <div className="row">
-                                 <div className="input-fiel col s12">
-                                    <input name="title" onChange={this.handleChange} type="text" placeholder="Task Title" />
-                                 </div>
-                              </div>
-                              <div className="row">
-                                 <div className="input-fiel col s12">
-                                    <textarea name="description" onChange={this.handleChange} placeholder="Task Description" className="materialize-textarea"></textarea>
-                                 </div>
-                              </div>
-                              <button type="submit" className="btn light-green darken-4">Send</button>
-                           </form>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="col s7">
-   
-                  </div>
-               </div>
+  addTask(e) {
+    //console.log(this.state);
+    e.preventDefault();
+    fetch("/api/tasks", {
+      method: "POST",
+      body: JSON.stringify(this.state),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+         console.log(data)
+         M.toast({html: 'Task Saved'})//Constant 'M' of materialize framework to use 'toast'
+         this.setState({title: '', description: ''})
+      })
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        {/* NAVIGATION */}
+        <nav className="light-blue darken-4">
+          <div className="container">
+            <a className="brand-logo" href="/">
+              MERN Stack
+            </a>
+          </div>
+        </nav>
+
+        <div className="container">
+          <div className="row">
+            <div className="col s5">
+              <div className="card">
+                <div className="card-content">
+                  <form onSubmit={this.addTask}>
+                    <div className="row">
+                      <div className="input-fiel col s12">
+                        <input
+                          name="title"
+                          onChange={this.handleChange}
+                          type="text"
+                          placeholder="Task Title" value={this.state.title}
+                        />
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="input-fiel col s12">
+                        <textarea
+                          name="description"
+                          onChange={this.handleChange}
+                          placeholder="Task Description"
+                          className="materialize-textarea" value={this.state.description}
+                        ></textarea>
+                      </div>
+                    </div>
+                    <button type="submit" className="btn light-green darken-4">
+                      Send
+                    </button>
+                  </form>
+                </div>
+              </div>
             </div>
-         </div>
-      )
-   }   
+            <div className="col s7"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
